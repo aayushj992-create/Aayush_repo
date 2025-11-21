74021ffd6d15b9a382714ec357d7c8a79fb8406c
Aayush Joshi <aayushj992@gmail.com>
2025-11-21 13:39:45 +0545

    ternary and logical operators added.

--- MathOperations.java
+++ MathOperations.java
@@ -14,21 +14,45 @@ public class MathOperations
         int a=scan.nextInt();
         System.out.println("Enter second number:");
         int b=scan.nextInt();
+        //Arithematic
         int sum=a+b;
         int diff=a-b;
         int mul=a*b;
         int div=a/b;
+        System.out.println("The sum is:"+sum);
+        System.out.println("The difference is:"+diff);
+        System.out.println("The Multiplication is:"+mul);
+        System.out.println("The division is:"+div);
+        //Urinary
         int pre_increment=++a;
         int post_increment=a++;
-        int 
-         greater=(a>b);
-        
-        
-        
-        System.out.println(sum);
-        System.out.println(diff);
-        System.out.println(mul);
-        System.out.println(div);
+        System.out.println("The pre_increment is:"+pre_increment);
+        System.out.println("The post_increment is:"+post_increment);
+        //Assignment
+        int c=10;
+        System.out.println("The value of c is:"+c);
+        //Relational
+        boolean same=(a==b);
+        System.out.println("a==b:"+same);
+        boolean greater_than=(a>=b);
+        System.out.println("a>=b:"+greater_than);
+        boolean less=(a<=b);
+        System.out.println("a<=b:"+less);
+        boolean great=(a>b);
+        System.out.println("a>b:"+less);
+        boolean lesser=(a<b);
+        System.out.println("a<b:"+lesser);
+        boolean not=(a!=b);
+        System.out.println("a!=b:"+not);
+        //Logical
+        boolean and=(a>b && a==b);
+        System.out.println("a>b && a==b:"+and);
+        boolean or=(a>b || a==b);
+        System.out.println("a>b || a==b:"+or);
+        //Ternary
+        String greater=(a>b)?"a is greatest":"b is greatest";
+        System.out.println(greater);
+     
         
         
     }
