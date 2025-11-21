1910c09cc0135d8183d871109f8ef8449ff16c7c
Aayush Joshi <aayushj992@gmail.com>
2025-11-21 13:16:24 +0545

    added operations.

--- MathOperations.java
+++ MathOperations.java
@@ -10,7 +10,26 @@ public class MathOperations
 {
     public static void main(String[] args){
         Scanner scan=new Scanner(System.in);
+        System.out.println("Enter first number:");
         int a=scan.nextInt();
+        System.out.println("Enter second number:");
+        int b=scan.nextInt();
+        int sum=a+b;
+        int diff=a-b;
+        int mul=a*b;
+        int div=a/b;
+        int pre_increment=++a;
+        int post_increment=a++;
+        int 
+         greater=(a>b);
+        
+        
+        
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(mul);
+        System.out.println(div);
+        
         
     }
 }
