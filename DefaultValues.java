1d9689cc8e9151d46a349d037878c86265f40439
Aayush Joshi <aayushj992@gmail.com>
2025-11-21 14:02:53 +0545

    showed default values.

---
+++ DefaultValues.java
@@ -0,0 +1,29 @@
+
+/**
+ * Write a description of class DefaultValues here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class DefaultValues
+{
+    byte a;
+    short b;
+    int c;
+    long d;
+    float e;
+    double f;
+    char g;
+    boolean h;
+    public static void main(String[] args){
+        DefaultValues t1=new DefaultValues();
+        System.out.println(t1.a);
+        System.out.println(t1.b);
+        System.out.println(t1.c);
+        System.out.println(t1.d);
+        System.out.println(t1.e);
+        System.out.println(t1.f);
+        System.out.println(t1.g);
+        System.out.println(t1.h);
+    }
+}
