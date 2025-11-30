60251c96e48df84d8209fdf6d3c1a2d283db09bd
Aayush Joshi <aayushj992@gmail.com>
2025-11-30 09:55:27 +0545

    semicolon added

--- income_tax.java
+++ income_tax.java
@@ -15,14 +15,14 @@ public class income_tax
         if(income<=500000)
         {
             float total_tax=income*0.01f;
-        System.out.println("The total tax is"+total_tax);
+        System.out.println("The total tax is:"+total_tax);
         }
         else if(income>500000 && income<=700000){
             float tax_1=500000*0.01f;
             float tax_2=income-500000;
             float tax_3=tax_2*0.1f;
             float total_tax=tax_1+tax_3+5000;
-            System.out.println("The total tax is"+total_tax);
+            System.out.println("The total tax is:"+total_tax);
         }
         else if(income>700000 && income<=1000000)
         {
@@ -31,7 +31,7 @@ public class income_tax
          float tax_3=income-700000;
          float tax_4=tax_3*0.2f;
          float total_tax=tax_1+tax_2+tax_4+15000;
-         System.out.println("The total tax is"+total_tax);
+         System.out.println("The total tax is:"+total_tax);
         }
         else if(income>1000000 && income<=2000000)
         { 
@@ -41,7 +41,7 @@ public class income_tax
          float tax_4=income-1000000;
          float tax_5=tax_4*0.3f;
          float total_tax=tax_1+tax_2+tax_3+tax_5+35000;
-         System.out.println("The total tax is"+total_tax);
+         System.out.println("The total tax is:"+total_tax);
         }
         else if(income>2000000 && income<=5000000)
         { 
@@ -52,7 +52,7 @@ public class income_tax
          float tax_5=income-3000000;
          float tax_6=tax_5*0.36f;
          float total_tax=tax_1+tax_2+tax_3+tax_4+tax_6+335000;
-         System.out.println("The total tax is"+total_tax);
+         System.out.println("The total tax is:"+total_tax);
         }
         else
         { 
@@ -64,7 +64,7 @@ public class income_tax
          float tax_6=income-5000000;
          float tax_7=tax_6*0.39f;
          float total_tax=tax_1+tax_2+tax_3+tax_4+tax_5+tax_7+1135000;
-         System.out.println("The total tax is"+total_tax);
+         System.out.println("The total tax is:"+total_tax);
         }    
 }
 }
